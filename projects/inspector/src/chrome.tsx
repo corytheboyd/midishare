fda76c9d5ac76fc0f1a352746777ab4f5ba0dffc
Cory Boyd <me@corytheboyd.com>
2021-01-10 13:10:19 -0800

    Page layout roughly

--- projects/inspector/src/chrome.tsx
+++ projects/inspector/src/chrome.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
 export const Chrome: React.FC = (props) => {
-  return <>{props.children}</>;
+  return (
+    <div className="flex flex-col h-full">
+      <nav className="bg-red-300">
+        <h1>MIDI Inspector</h1>
+      </nav>
+      <main className="flex-grow bg-blue-300">
+        <section className="">{props.children}</section>
+      </main>
+      <footer className="bg-yellow-300">
+        <span>Footer content</span>
+      </footer>
+    </div>
+  );
 };
