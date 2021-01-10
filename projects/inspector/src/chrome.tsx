c16e1d02e567b700a96f7ebcb1d7c362a14b518d
Cory Boyd <me@corytheboyd.com>
2021-01-10 13:22:47 -0800

    Rest of page styling

--- projects/inspector/src/chrome.tsx
+++ projects/inspector/src/chrome.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 export const Chrome: React.FC = (props) => {
   return (
     <div className="flex flex-col h-full">
-      <nav className="bg-red-300">
-        <h1>MIDI Inspector</h1>
+      <nav className="bg-gray-800 text-gray-200 px-3 py-2">
+        <h1 className="font-mono font-medium font-thin">MIDI Inspector</h1>
       </nav>
-      <main className="flex-grow bg-blue-300">
-        <section className="">{props.children}</section>
+      <main className="bg-gray-200 text-gray-800 flex-grow">
+        {props.children}
       </main>
-      <footer className="bg-yellow-300">
-        <span>Footer content</span>
+      <footer className="bg-gray-300 text-gray-500 px-5 py-5 text-center">
+        <span className="text-sm">© Midishare • 2021</span>
       </footer>
     </div>
   );
