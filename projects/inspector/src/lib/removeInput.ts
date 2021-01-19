c9f0f43808ed60d04f0d92425a0a492a94a0daee
Cory Boyd <me@corytheboyd.com>
2021-01-18 18:43:08 -0800

    Fix removed device bug
    Input reference was removed before attempt at
    letting go of event listeners.

--- projects/inspector/src/lib/removeInput.ts
+++ projects/inspector/src/lib/removeInput.ts
@@ -1,11 +1,11 @@
 import { DeviceId, store } from "./store";
 
 export function removeInput(id: DeviceId): void {
-  const removeInput = store.getState().removeInput;
-  removeInput(id);
-
   // Removes all listeners. If this input comes back, it will have listeners
   // registered again.
   const input = store.getState().inputs[id];
   input.removeListener();
+
+  const removeInput = store.getState().removeInput;
+  removeInput(id);
 }
