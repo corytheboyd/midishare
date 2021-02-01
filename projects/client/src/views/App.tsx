5e53360282252b9aaa7d39c22815666fc447c59a
Cory Boyd <me@corytheboyd.com>
2021-02-01 12:04:34 -0800

    Just a mock landing page

--- projects/client/src/views/App.tsx
+++ projects/client/src/views/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Chrome } from "./Chrome";
+import { Chrome, MaxWidthContent } from "./Chrome";
 
 export const App: React.FC = () => {
   return (
     <Chrome>
-      <p>mad content yo</p>
+      <article className="flex flex-col items-center">
+        <div className="text-center pb-5 max-w-3xl">
+          <h1 className="font-serif font-bold text-5xl mt-8">
+            Live music sessions with real-time MIDI input streaming
+          </h1>
+          <p className="mt-8 text-xl">
+            Lorem ipsum lorem ipsum ya hurd. Bird up!
+          </p>
+        </div>
+
+        <MaxWidthContent></MaxWidthContent>
+      </article>
     </Chrome>
   );
 };
