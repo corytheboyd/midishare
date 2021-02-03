63ef4ac09d2ea4444ddaaa62589007ea536be4e8
Cory Boyd <me@corytheboyd.com>
2021-02-03 13:39:10 -0800

    Add common directory to share code between client/server

---
+++ projects/common/README.md
@@ -0,0 +1,9 @@
+# Common
+
+Shared code between projects.
+
+## Disclaimer
+
+While cool, absolutely avoid overdoing code sharing. It's so easy to build yourself into an absolute trash heap of a codebase.
+
+I promise to do my best with this :)
---
+++ projects/common/rollup.config.js
@@ -0,0 +1,22 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "es",
+    sourcemap: true,
+    preserveModules: true,
+  },
+  watch: {
+    clearScreen: false,
+  },
+  external: [],
+  plugins: [
+    nodeResolve({ moduleDirectories: ["node_modules"] }),
+    commonjs(),
+    typescript(),
+  ],
+};
