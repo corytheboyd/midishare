e05eebc6638a04101ded7bd60cfc1dada8cbc555
Cory Boyd <me@corytheboyd.com>
2021-02-15 10:27:15 -0800

    Ensure path exists in tmp before process

--- projects/server-nginx/script/replace-variables-dir.sh
+++ projects/server-nginx/script/replace-variables-dir.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -eo pipefail
 
+# See replace-variables.sh. Does the same thing, but recursively
+# for all files in a source directory, written to a target
+# directory.
+#
+# By default, if only a SOURCE_DIR is given, the files in that
+# directory will be modified in place. This is a bit of a risky
+# default behavior, I chose it because it fit my exact use-case
+# at the time.
+#
+# Requires the script replace-variables.sh be in the same
+# directory as this script.
+
 # Enable **/*
 shopt -s globstar dotglob;
 
 # https://stackoverflow.com/a/246128/15193865
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# See replace-variables.sh. Does the same thing, but recursively
-# for all files in a source directory, written to a target
-# directory.
 
 function usage() {
   echo "Usage: $(basename "$0") [SOURCE_DIR] [...SOURCE_DIR]" >&2
@@ -38,6 +46,8 @@ function process_dir() {
   # https://unix.stackexchange.com/a/139381
   while IFS= read -r -d '' -u 9
   do
+    # Ensure subpath exists in the tmp directory first
+    mkdir -p "$TMP_DIR$(dirname "$REPLY")"
     process_file "$REPLY"
   done 9< <( find "$SOURCE_DIR" -type f -exec printf '%s\0' {} + )
 }
