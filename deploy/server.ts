655e9c22ed229159b799634c6a99cdf2821b6f2b
Cory Boyd <me@corytheboyd.com>
2021-02-17 12:58:37 -0800

    Unpack dist.tgz and specify run path

--- deploy/server.ts
+++ deploy/server.ts
@@ -2,30 +2,38 @@ import { resolve } from "path";
 import { runCommand } from "./lib/runCommand";
 import { sendToServer } from "./lib/sendToServer";
 import { ARTIFACT_ROOT } from "./index";
+import { execOnServer } from "./lib/execOnServer";
+
+const DIST_FILE = "dist.tgz";
 
 (async () => {
   // Run everything from the project root directory
   process.chdir(resolve(__dirname, ".."));
 
-  console.log("Cleanup build artifacts");
+  console.log("Cleanup build artifacts...");
   await runCommand([`rm -rf ${resolve(ARTIFACT_ROOT, "server")}`]);
   await runCommand([`mkdir -p ${resolve(ARTIFACT_ROOT, "server")}`]);
 
-  console.log("Create production build images");
+  console.log("Create production build image...");
   await runCommand(["bin/dcp-prod build", "server"]);
 
-  console.log("Building server dist....");
+  console.log("Building dist....");
   await runCommand([
     "bin/dcp-prod",
     "run",
     "--rm",
     `--volume=${resolve(ARTIFACT_ROOT, "server")}:/artifacts`,
     "server",
-    `sh -c "cd dist && tar -zcvf ../dist.tgz . && cd - ; cp dist.tgz /artifacts ; chown -R $(id -u):$(id -g) /artifacts"`,
+    `sh -c "cd dist && tar -zcvf ../${DIST_FILE} . && cd - ; cp ${DIST_FILE} /artifacts ; chown -R $(id -u):$(id -g) /artifacts"`,
   ]);
 
-  console.log("Sending server dist to host...");
+  console.log("Sending dist to host...");
   await sendToServer(resolve(ARTIFACT_ROOT, "server"), "/home/node/dist");
+
+  console.log("Unpacking dist on host...");
+  await execOnServer([
+    `tar -xvf /home/node/dist/server/${DIST_FILE} ; rm /home/node/dist/server/${DIST_FILE}`,
+  ]);
 
   console.log("Running PM2 deployment...");
   await runCommand([
--- projects/server/index.js
+++ projects/server/index.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === "development") {
   require("ts-node/register");
   require("./src/index");
 } else if (process.env.NODE_ENV === "production") {
-  require("./dist/index.js");
+  require("/home/node/dist/server/index.js");
 } else {
   throw new Error(`Unexpected NODE_ENV: ${process.env.NODE_ENV}`);
 }
