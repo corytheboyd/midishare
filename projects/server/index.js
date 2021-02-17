70b6cb294a032550583ac6631ffd8dfeaba86273
Cory Boyd <me@corytheboyd.com>
2021-02-17 13:14:40 -0800

    test

--- projects/server/index.js
+++ projects/server/index.js
@@ -5,7 +5,8 @@ if (process.env.NODE_ENV === "development") {
   require("ts-node/register");
   require("./src/index");
 } else if (process.env.NODE_ENV === "production") {
-  require("/home/node/dist/server/index.js");
+  // require("/home/node/dist/server/index.js");
+  console.log("hahahah run prod");
 } else {
   throw new Error(`Unexpected NODE_ENV: ${process.env.NODE_ENV}`);
 }
