d3f74702eef9d500c5fd60112210899a27066799
Cory Boyd <me@corytheboyd.com>
2021-02-17 13:17:30 -0800

    dotenv is a prod dependency

--- projects/server/index.js
+++ projects/server/index.js
@@ -5,8 +5,7 @@ if (process.env.NODE_ENV === "development") {
   require("ts-node/register");
   require("./src/index");
 } else if (process.env.NODE_ENV === "production") {
-  // require("/home/node/dist/server/index.js");
-  console.log("hahahah run prod");
+  require("/home/node/dist/server/index.js");
 } else {
   throw new Error(`Unexpected NODE_ENV: ${process.env.NODE_ENV}`);
 }
