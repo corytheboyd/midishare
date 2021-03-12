07dd0f1e3b7d63e633f226e8ea3621ab8a9f283f
Cory Boyd <me@corytheboyd.com>
2021-03-12 13:56:55 -0700

    Split public/protected CDN routes

--- projects/client/src/lib/constants.ts
+++ projects/client/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROTECTED_CDN_URL = (() => {
-  const url = new URL(process.env.CDN_URL as string);
+  const url = new URL(process.env.PROTECTED_CDN_URL as string);
   url.pathname = "/protected";
   return url.toString();
 })();
