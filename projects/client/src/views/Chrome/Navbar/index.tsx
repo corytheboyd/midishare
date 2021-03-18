92ec005b32e60b506c47812f76a3ac810cb2f937
Cory Boyd <me@corytheboyd.com>
2021-03-18 16:36:43 -0600

    Decouple currentUser image out from login/logout

--- projects/client/src/views/Chrome/Navbar/index.tsx
+++ projects/client/src/views/Chrome/Navbar/index.tsx
@@ -30,50 +30,16 @@ export const NavButton: React.FC<BaseButtonProps> = (props) => (
   </BaseButton>
 );
 
-const ProfileSection: React.FC = () => {
-  const { isLoading, data } = useQuery<UserProfile | null>(
+export const Navbar: React.FC = () => {
+  const currentUserQuery = useQuery<UserProfile | null>(
     queryKey(),
-    () => getCurrentUser(),
+    getCurrentUser,
     {
       refetchInterval: false,
       refetchOnWindowFocus: false,
     }
   );
 
-  if (isLoading) {
-    return null;
-  }
-
-  if (!data) {
-    return (
-      <NavButton href={LOGIN_URL}>
-        <div className="flex space-x-0.5 items-center">
-          <div className="w-4 h-4 text-gray-500">
-            <Login />
-          </div>
-          <div>Login</div>
-        </div>
-      </NavButton>
-    );
-  }
-
-  return (
-    <div className="flex items-center space-x-1">
-      <img className="w-6 h-6 rounded" src={data.picture} alt={data.name} />
-
-      <NavButton href={LOGOUT_URL}>
-        <div className="flex space-x-0.5 items-center">
-          <div className="w-4 h-4 text-gray-500">
-            <Logout />
-          </div>
-          <div>Logout</div>
-        </div>
-      </NavButton>
-    </div>
-  );
-};
-
-export const Navbar: React.FC = () => {
   return (
     <nav className="flex flex-col py-2 px-3 lg:p-3 bg-gray-700 shadow-lg">
       <MaxWidthContent>
@@ -84,7 +50,17 @@ export const Navbar: React.FC = () => {
             </a>
           </h1>
 
-          <div className="flex-grow flex justify-end">
+          <div className="flex-grow flex items-center justify-end space-x-2">
+            {currentUserQuery.data && (
+              <div>
+                <img
+                  className="h-6 w-6 rounded"
+                  src={currentUserQuery.data.picture}
+                  alt={currentUserQuery.data.name}
+                />
+              </div>
+            )}
+
             <NavButton>
               <div className="w-5 h-5">
                 <Menu />
