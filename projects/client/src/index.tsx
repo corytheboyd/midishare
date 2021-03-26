3de9096e8bf7e9f06cdb0e8b5257ca8f81422ad5
Cory Boyd <me@corytheboyd.com>
2021-03-26 13:58:00 -0600

    Add release and env to client

--- projects/client/src/index.tsx
+++ projects/client/src/index.tsx
@@ -23,6 +23,9 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+
+  release: process.env.GIT_REV || process.env.NODE_ENV,
+  environment: process.env.NODE_ENV,
 });
 
 render(
